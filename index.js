7e1532ee84ce895fabdd054ddc05896dbcda146c
Barry Johnson <bdotmgmt@gmail.com>
2021-05-18 13:36:43 -0400

    Install bcrypt and create hash

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const plainText = 'ReskillAmericans123';
+const hashedPw = ''
+
+
+bcrypt.hash(plainText, saltRounds)
+    .then(hash => {
+        hashedPw += hash;
+    });
 
 
 app.listen(port, () => {
