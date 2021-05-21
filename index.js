4789b1d2986b315073521de266c4101afb67eb0c
Barry Johnson <bdotmgmt@gmail.com>
2021-05-20 20:28:54 -0400

    Refactor post route to throw error message if post request does not include password

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const port = 4000;
 const bcrypt = require('bcrypt');
 const saltRounds = 10; // USED TO CREATE hashedPw
 const plainText = 'ReskillAmericans123'; // USED TO CREATE hashedPw
-
-const hashedPw = '$2b$10$c19r1NhJoRZq0S5LrE.CzOc4NLKLk9xW9pNB7Qwq88jqba7WSG6.6'
+let hashedPw;
 
 
 app.use(express.json());
 
-/*----------------------------------------------------------------
+
 
 // CREATED HASHED PW THEN STORED IN hashedPw VARIABLE
 
@@ -18,11 +17,9 @@ bcrypt.hash(plainText, saltRounds, (err, hash) => {
     if (err) {
         return console.error(err);
     } else {
-        return console.log(hash);
+        hashedPw = hash;
     }
 })
-
-----------------------------------------------------------------*/
 
 
 // CREATED POST ROUTE @ '/login' 
@@ -30,13 +27,17 @@ bcrypt.hash(plainText, saltRounds, (err, hash) => {
 app.post('/login', (req, res) => {
     const pass = req.body.pass;
     // IF pass === hashedPw RETURN A SUCCESSFUL LOGIN MESSAGE. IF NOT, PW IS INCORRECT
-    bcrypt.compare(pass, hashedPw, (err, match) => {
-        if (!match) {
-            return res.status(500).json({err: "Incorrect password. Please re-enter your password.", match});
-        } else {
-            return res.status(200).json({ message: "Sucessfully logged in!", match});
-        }
-    })
+    if (!pass) {
+        return res.status(500).json({message: 'Must enter a password to login'});
+    } else {
+        bcrypt.compare(pass, hashedPw, (err, match) => {
+            if (!match) {
+                return res.status(500).json({err: "Incorrect password. Please re-enter your password.", match});
+            } else {
+                return res.status(200).json({ message: "Sucessfully logged in!", match});
+            }
+        })
+    }
 })
 
 
